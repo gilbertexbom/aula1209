8a81a7b706f67ce84f00712a6ab2561606a97725
Gilberto <gilberto.braga0021@gmail.com>
2023-09-18 08:51:45 -0300

    Curso de python do tio paza

---
+++ exe5.py
@@ -0,0 +1,10 @@
+#recebe um valor e testa
+
+
+teste = input('Informe um dado: ')
+print(type(teste))
+
+print('É número? ', teste.isnumeric())
+print('É alfabético? ', teste.isalpha())
+print('É alfanumérico? ', teste.isalpha())
+
